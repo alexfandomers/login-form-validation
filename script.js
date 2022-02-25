b4682a1f5cd4e497de696882cac6d10326128328
alexfandomers <71307225+alexfandomers@users.noreply.github.com>
2022-02-25 16:17:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,50 @@
-//let's get all required elemnts
-const form = document.querySelector("form"),
+const form = document.querySelector("form");
 eField = form.querySelector(".email"),
 eInput = eField.querySelector("input"),
 pField = form.querySelector(".password"),
 pInput = pField.querySelector("input");
 
-form.onsubmit = (e) => {
-    e.preventDefault(); //preventing form from submiting 
-    if(eInput.value == ""){ //if email is empty
-        eField.classList.add("shake", "error");
-    }else{
-        checkEmail(); //calling checkEMmail function
-    }
-    if(pInput.value == ""){ //if password is empty
-        pField.classList.add("shake", "error");
-    }
+form.onsubmit = (e)=>{
+  e.preventDefault(); //preventing from form submitting
+  //if email and password is blank then add shake class in it else call specified function
+  (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
+  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
 
-    setTimeout(() =>{ //remove shake class after 500ms
-        eField.classList.remove("shake")
-        pField.classList.remove("shake")
-    }, 500);   
+  setTimeout(()=>{ //remove shake class after 500ms
+    eField.classList.remove("shake");
+    pField.classList.remove("shake");
+  }, 500);
 
-    //lets work on input keyup
-    eInput.onkeyup = ()=>{
-        checkEmail(); //calling chekEmail function
+  eInput.onkeyup = ()=>{checkEmail();} //calling checkEmail function on email input keyup
+  pInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
+
+  function checkEmail(){ //checkEmail function
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate email
+    if(!eInput.value.match(pattern)){ //if pattern not matched then add error and remove valid class
+      eField.classList.add("error");
+      eField.classList.remove("valid");
+      let errorTxt = eField.querySelector(".error-txt");
+      //if email value is not empty then show please enter valid email else show Email can't be blank
+      (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+    }else{ //if pattern matched then remove error and add valid class
+      eField.classList.remove("error");
+      eField.classList.add("valid");
     }
+  }
 
-    //lets create a function
-    function checkEmail(){
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;//pattern to validate email
-        if(!eInput.value.match(pattern)){ //if pattern not matched with user entered value
-            eField.classList.add("error");
-            let errorTxt = eField.querySelector(".error-txt"); 
-            //if email is not empty then Enter a valid email adress until valid else show email can't be blank
-            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email adress": errorTxt.innerText ="Email cant be blank";
-
-        }else{
-            eField.classList.remove("error");
-        }
+  function checkPass(){ //checkPass function
+    if(pInput.value == ""){ //if pass is empty then add error and remove valid class
+      pField.classList.add("error");
+      pField.classList.remove("valid");
+    }else{ //if pass is empty then remove error and add valid class
+      pField.classList.remove("error");
+      pField.classList.add("valid");
     }
-    
-    eInput.onkeyup = ()=>{
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;//pattern to validate email
-        if(!eInput.value.match(pattern)){ //if pattern not matched with user entered value
-            eField.classList.add("error");
-            let errorTxt = eField.querySelector(".error-txt"); 
-            //if email is not empty then Enter a valid email adress until valid else show email can't be blank
-            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email adress": errorTxt.innerText ="Email cant be blank";
+  }
 
-        }else{
-            eField.classList.remove("error");
-        }
-    }
-
-    pInput.onkeyup = ()=>{        
-        if(pInput.value){ //if pattern not matched with user entered value
-            pField.classList.add("error");
-        }else{
-            pField.classList.remove("error");
-        }
-    }
-//let wor on what to do after user filled up proper details
-//if error class not contains in eField an pField the user entered proper details
-if(!eField.classList.contains("error") && !pField.classList.contains("error")){
-    window.location.href = "#"; //remove that # and put url where you want to submit the form data
+  //if eField and pField doesn't contains error class that mean user filled details properly
+  if(!eField.classList.contains("error") && !pField.classList.contains("error")){
+    window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
     console.log("Form Submited")
-}
+  }
 }
